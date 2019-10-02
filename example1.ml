6e088bf679296f6746ca0d2b04490e43bbc879fa
Francesco Mecca <me@francescomecca.eu>
2019-10-02 16:34:38 +0200

    example1

--- example1.ml
+++ example1.ml
@@ -1,15 +1,10 @@
-type t = K1 of int | K2 of bool | K3 of int| K4
+type p = P0 of int;;
+type t = K1 of int | K2 of bool | K3 of int| K4 | K5 of p
 
 let f t = "t"
 
-let a = fun lt -> match lt with
-  | [K1 0; _] as t -> f t  (* tag 0 *)
-  | [K1 1; _] -> "K1 1" (* tag 0 *)
-  | [_;K1 1] -> "K1 1" (* tag 0 *)
-  | [_; K1 _] -> "K1 _" (* tag 0 *)
-  | [K2 false; _] -> "K2 false"(* tag 1 *)
-  | [K2 _; _] -> "K2 _"(* tag 1 *)
-  | [_; K2 _] -> "K2 _"(* tag 1 *)
-  | [K3 t] when t == 2 -> "K3 2"(* tag 2 *)
-  | [K4] -> "K4" (* int 0 *)
-  | _ -> "[_, _]"
+let a = fun t -> match t with
+  | K4 -> "K4"
+  | K1 2 -> "K1 2"
+  | K1 _ -> "K1 _"
+  | _ -> "wildcard"
