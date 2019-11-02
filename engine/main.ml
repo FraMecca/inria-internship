c790563fffc3a22dfd1f977e9b6a4292bc8d1cdd
Francesco Mecca <me@francescomecca.eu>
2019-11-02 12:13:45 +0100

    modified sexpr definition

--- engine/main.ml
+++ engine/main.ml
@@ -35,11 +35,11 @@ and
   | Int of int
   | Bool of bool
   | Function of variable * sexpr
-  | Let of variable * sexpr * sexpr
+  | Let of variable * sexpr
   | Catch of sexpr * exitpoint * variable list * sexpr
   | Exit of exitpoint * sexpr list (* could be "exit 1 var1 var2" *)
   | If of bexpr * sexpr * sexpr
-  | Switch of variable * switch_case list * sexpr option
+  | Switch of sexpr * switch_case list * sexpr option
   | Field of int * variable
   | Comparison of bop * sexpr * int
   | TBlackbox of target_blackbox
