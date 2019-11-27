47694933113cc067332c4bc88ec55438eca597f2
Francesco Mecca <me@francescomecca.eu>
2019-11-27 18:35:18 +0100

    environment now a record of {symbolic-values; exitpoints; functions}

--- engine/main.ml
+++ engine/main.ml
@@ -281,25 +281,29 @@ let example_ast =
   parse_file Sys.argv.(1)
 
 module SMap = Map.Make(String)
+module IMap = Map.Make(struct type t = int let compare = compare end)
 
 type constraint_tree =
   | Leaf of pi list * target_blackbox
   | Node of constraint_tree list
 and
-  pi = { var: accessor; op: piop } (* record of a variable and a constraint on that variable *)
+  pi = { var: sym_value; op: piop } (* record of a variable and a constraint on that variable *)
 and
-  symbolic_value =
-  | Accessor of accessor
-  | Addition of int * accessor
-  | Function of variable * constraint_tree 
-  | Catch of exitpoint * variable list * constraint_tree
+  sym_function = variable * constraint_tree
 and
-  environment = symbolic_value SMap.t
+  sym_catch = exitpoint * variable list * constraint_tree
 and
-  accessor =
+  environment = {
+  values: sym_value SMap.t;
+  functions: sym_function SMap.t;
+  exits: sym_catch IMap.t;
+}
+and
+  sym_value =
   | AcRoot of variable
-  | AcField of accessor * int
-  | AcTag of accessor * int
+  | AcField of sym_value * int
+  | AcTag of sym_value * int
+  | AcAdd of sym_value * int
 and
   piop =
   | Tag of int
@@ -349,21 +353,23 @@ let rec sym_exec sexpr constraints env : constraint_tree =
     in
     extract [] tree
   in
-  let rec subst_accessor bindings = function
+  let rec subst_svalue bindings = function
     | AcRoot v -> begin
         match List.assoc_opt v bindings with
-        | Some acc -> acc
+        | Some svalue -> svalue
         | None -> AcRoot v
       end
-    | AcField (acc', i) -> AcField (subst_accessor bindings acc', i)
-    | AcTag (acc', i) -> AcTag (subst_accessor bindings acc', i)
+    | AcField (acc', i) -> AcField (subst_svalue bindings acc', i)
+    | AcTag (acc', i) -> AcTag (subst_svalue bindings acc', i)
+    | AcAdd (svalue', i) -> AcAdd (subst_svalue bindings svalue', i)
   in
   let bprintf = Printf.bprintf
   in
-  let rec bprint_accessor buf = function
+  let rec bprint_svalue buf = function
     | AcRoot (v) -> bprintf buf "AcRoot=%s" v
-    | AcField (a, i) -> bprintf buf "AcField(%d %a)" i bprint_accessor a
-    | AcTag (a, i) -> bprintf buf "AcTag(%d %a)" i bprint_accessor a
+    | AcField (a, i) -> bprintf buf "AcField(%d %a)" i bprint_svalue a
+    | AcTag (a, i) -> bprintf buf "AcTag(%d %a)" i bprint_svalue a
+    | AcAdd (a, i) -> bprintf buf "AcAdd(%d %a)" i bprint_svalue a
   in
   let bprint_pi buf pi =
     let print_op buf = function
@@ -380,7 +386,7 @@ let rec sym_exec sexpr constraints env : constraint_tree =
       | Isout i -> bprintf buf "Isout %d" i
       | Isin i -> bprintf buf "Isin %d" i
     in
-    bprintf buf "{ var=%a; op=%a; }" bprint_accessor pi.var print_op pi.op
+    bprintf buf "{ var=%a; op=%a; }" bprint_svalue pi.var print_op pi.op
   in
   let rec bprint_list ~sep bprint buf = function
     | [] -> ()
@@ -409,64 +415,76 @@ let rec sym_exec sexpr constraints env : constraint_tree =
          (bprint_list ~sep (bprint_tree (ntabs+1))) cst_list
   and
   bprint_env ntabs buf env =
-    let bprint_binding buf (key, entry) =
-      let bprint_value buf = function
-        | Accessor a -> bprint_accessor buf a
-        | Addition (i, a) ->
-           bprintf buf "Addition=%d,%a" i bprint_accessor a
-        | Function (v, f_tree) ->
-           bprintf buf  "Function=%s,ConstraintTree: %a"
-             v
-             (bprint_tree (ntabs + 1)) f_tree
-        | Catch (e, vars, c_tree) ->
-          bprintf buf "Catch=%d %s,ConstraintTree: %a"
-            e
-            (String.concat " " vars)
-             (bprint_tree (ntabs + 1)) c_tree
+    let bprint_function buf binding =
+      let (k, fn) = binding in
+        let (v, f_tree) = fn
+        in
+        bprintf buf "%s: Function=%s,ConstraintTree: %a"
+          k v
+        (bprint_tree (ntabs + 1)) f_tree
       in
-      bprintf buf "%s: %a" key bprint_value entry
+      let bprint_exits buf binding =
+      let (k, catch) = binding in
+        let (e, vars, c_tree) = catch
+        in
+        bprintf buf "%d: Catch=%d %s,ConstraintTree: %a"
+          k e
+          (String.concat " " vars)
+          (bprint_tree (ntabs + 1)) c_tree
     in
-    bprint_list ~sep:(break ntabs) bprint_binding buf (SMap.bindings env)
+    let bprint_svalues buf binding =
+      let (k, svalue) = binding in
+        bprintf buf "%s: %a" k bprint_svalue svalue
+      in
+    bprint_list ~sep:(break ntabs) bprint_function buf (SMap.bindings env.functions);
+    bprint_list ~sep:(break ntabs) bprint_exits buf (IMap.bindings env.exits);
+    bprint_list ~sep:(break ntabs) bprint_svalues buf (SMap.bindings env.values)
   in
   let print_env env =
     let buf = Buffer.create 42 in
     bprint_env 0 buf env;
     BatIO.write_line BatIO.stdout (Buffer.contents buf)
   in
-  let expand_env variable entry : environment =
-    assert (not (SMap.mem variable env));
-    SMap.add variable entry env
+  let put_function variable fn : environment =
+    assert (not (SMap.mem variable env.functions));
+    {env with functions = SMap.add variable fn env.functions }
+  in
+  let put_exit variable ext : environment =
+    assert (not (IMap.mem variable env.exits));
+    {env with exits = IMap.add variable ext env.exits }
+  in
+  let put_value variable value : environment =
+    assert (not (SMap.mem variable env.values));
+    {env with values = SMap.add variable value env.values }
   in
   (* perform union on two maps, keys should never differ *)
-  let union env1 env2 = SMap.union (fun _ a b -> assert (a = b); Some a) env1 env2
+  let union env1 env2 = {
+    values=SMap.union (fun _ a b -> assert (a = b); Some a) env1.values env2.values;
+    functions=SMap.union (fun _ a b -> assert (a = b); Some a) env1.functions env2.functions;
+    exits=IMap.union (fun _ a b -> assert (a = b); Some a) env1.exits env2.exits;
+  }
+      
   in
-  let eval_let_binding env acc =
-    match acc with
-    | Var v -> Accessor (AcRoot v)
+  let eval_let_binding env sxp key =
+    match sxp with
+    | Var v ->
+      put_value v (AcRoot v)
     | Field (i, v) ->
-      let acc = match SMap.find_opt v env with
-        | Some (Accessor a) -> a
-        | Some (Addition _) -> assert false
-        | Some (Function _) -> assert false
-        | Some (Catch _) -> assert false
-        | None -> assert false
-      in
-      Accessor (AcField (acc, i))
+      let acc = SMap.find v env.values in
+      put_value key (AcField (acc, i))
+    | Addition (i, v) ->
+      let svalue = SMap.find v env.values in
+      put_value key (AcField (svalue, i))
     | Function (v, sxp) ->
-      let env' = expand_env v (Accessor (AcRoot v))
-      in
-      let constraint_tree = sym_exec sxp constraints env' in
-      Function (v, constraint_tree)
-    | Addition (i, v) -> begin match SMap.find_opt v env with
-        | Some (Accessor a) -> Addition (i, a)
-        | _ -> assert false
-      end
+      let envf = put_value v (AcRoot v) in
+      let c_tree = sym_exec sxp constraints envf in
+      put_function key (v, c_tree)
     | _ -> assert false
   in
   match sexpr with
   | Let (blist, next_sexpr) ->
     let env' = blist |>
-               List.map (fun (var, sxp) -> expand_env var (eval_let_binding env sxp)) |>
+               List.map (fun (var, sxp) -> eval_let_binding env sxp var) |>
                List.fold_left union env
     in
     sym_exec next_sexpr constraints env'
@@ -478,12 +496,7 @@ let rec sym_exec sexpr constraints env : constraint_tree =
       | _ -> assert false (* TODO *)
     in
     let avar = match sxp with
-      | Var v -> begin
-          match SMap.find_opt v env with
-          | Some (Accessor a) -> a
-          | Some (Addition _) -> assert false
-          | _ -> assert false
-        end
+      | Var v -> SMap.find v env.values
       | _ -> assert false
     in
     let child1 = sym_exec strue ({var=avar; op=piop}::constraints) env
@@ -493,11 +506,7 @@ let rec sym_exec sexpr constraints env : constraint_tree =
     Node [child1; child2;]
   | Switch (sxp, swlist, defcase) ->
     let var = match sxp with
-      | Var v -> begin
-          match SMap.find_opt v env with
-        | Some (Accessor a) -> a
-        | _ -> assert false
-        end
+      | Var v -> SMap.find v env.values
       | _ -> assert false
     in
     let constraintsxtargets = List.map (fun (test, sxp) -> (match_switch test, sxp)) swlist
@@ -516,38 +525,36 @@ let rec sym_exec sexpr constraints env : constraint_tree =
     end
   | Catch (sxp, extpt, varlist, exit_sxp) ->
     let c_tree = sym_exec exit_sxp constraints env in
-    let env' = expand_env (string_of_int extpt) (Catch (extpt, varlist, c_tree)) in
+    let env' = put_exit extpt (extpt, varlist, c_tree) in
     sym_exec sxp constraints env'
   | Exit (ext, evalues) ->
     let innervars = evalues |> List.map (fun v -> match v with
         | Var inner -> inner
         | _ -> assert false)
     in
-    let inneraccs = innervars |> List.map(fun v -> match SMap.find_opt v env with
-        | Some (Accessor a) -> a
-        | _ -> assert false)
+    let innersvalues = innervars |> List.map (fun v -> SMap.find v env.values)
     in
-    let branch = match SMap.find_opt (string_of_int ext) env with
-      | Some (Catch (ext', vars, c_tree)) -> assert (ext' = ext);
-        assert (List.length vars = List.length inneraccs);
-        let bindings = List.combine vars inneraccs
-        in
-        let leaves = extract_leaves c_tree
-        in
-        let new_leaves = leaves |>
-                         List.map (function
-                             | Leaf (pis, t) ->
-                               let pis' = List.map
-                                   (fun pi -> { pi with var = subst_accessor bindings pi.var }) pis
-                               in
-                               Leaf (pis', t)
-                             | _ -> assert false)
-        in
-        if (List.length new_leaves) = 1 then
-          List.hd new_leaves
-        else
-          Node new_leaves
-      | _ -> assert false
+    let branch =
+      let (ext', vars, c_tree) = IMap.find ext env.exits in
+      assert (ext' = ext);
+      assert (List.length vars = List.length innersvalues);
+      let bindings = List.combine vars innersvalues
+      in
+      let leaves = extract_leaves c_tree
+      in
+      let new_leaves = leaves |>
+                       List.map (function
+                           | Leaf (pis, t) ->
+                             let pis' = List.map
+                                 (fun pi -> { pi with var = subst_svalue bindings pi.var }) pis
+                             in
+                             Leaf (pis', t)
+                           | _ -> assert false)
+      in
+      if (List.length new_leaves) = 1 then
+        List.hd new_leaves
+      else
+        Node new_leaves
     in
     branch
   | String s -> print "Leaf String"; Leaf (constraints, s)
@@ -556,6 +563,9 @@ let rec sym_exec sexpr constraints env : constraint_tree =
     Leaf (constraints, t)
   | _ -> assert false
 
+let empty_environment () =
+  { values=SMap.empty; functions=SMap.empty; exits=IMap.empty; }
+
 let () =
-  let _ = sym_exec example_ast [] SMap.empty in
+  let _ = sym_exec example_ast [] (empty_environment ())  in
   ()
