93f4659c38c53b3834c0d85b2d7a46e2f0085b1a
Francesco Mecca <me@francescomecca.eu>
2020-01-16 16:27:24 +0100

    preliminary fixes

--- engine/source_sym_engine.ml
+++ engine/source_sym_engine.ml
@@ -1,8 +1,12 @@
 open Ast
 
 type constraint_tree =
+  | Empty
   | Leaf of source_expr
-  | Node of (constructor * constraint_tree) list * constraint_tree option
+  | Node of (constructor * constraint_tree) list * constraint_tree
+
+let string_of_source_expr = function
+  | SBlackbox s -> s
 
 let print_result stree =
   let bprintf = Printf.bprintf
@@ -24,17 +28,18 @@ let print_result stree =
       | Variant s -> bprintf buf "Variant %s" s
       | Int i -> bprintf buf "Int %d" i
       | Bool b -> bprintf buf "Bool %b" b
-      | String s -> bprintf buf "String %s" s
+      | String s -> bprintf buf "String \"%s\"" s
       | Tuple narity -> bprintf buf "Tuple[%d]" narity
       | Nil ->  bprintf buf "Nil"
       | Cons -> bprintf buf "Cons"
     in
     match tree with
+    | Empty -> bprintf buf "Empty"
     | Leaf expr ->
       bprintf buf
         "Leaf='%a'"
         bprint_source_expr expr
-    | Node (k_cst_list, cst_opt) ->
+    | Node (k_cst_list, fallback_cst) ->
       bprintf buf "Node:{\
                    %a \
                    %t} Fallback: %a"
@@ -46,10 +51,7 @@ let print_result stree =
                (bprint_tree (ntabs+1)) cst))
         k_cst_list
         (break ntabs)
-        (fun buf -> begin match cst_opt with 
-             | Some cst -> fun _ -> bprint_tree (ntabs+1) buf cst
-             | None -> fun _ -> bprintf buf "None" 
-           end) buf
+        (bprint_tree (ntabs+1)) fallback_cst
   in
   let buf = Buffer.create 42 in
   bprint_tree 0 buf stree;
@@ -57,65 +59,116 @@ let print_result stree =
 
 type row = pattern list * source_expr
 
+type hashable_constructor = | HConstructor of constructor | HWildcard
+
 let sym_exec source =
-  let kst_rows_assoc rows : (constructor * row list) list * row option =
-    let hashtbl = Hashtbl.create 42
+  let group_constructors rows : (constructor * row list) list * row list =
+    let grouptbl = Hashtbl.create 42
     in
-    let with_wildcard = ref []
+    let (ksttbl: (source_blackbox , int) Hashtbl.t) = Hashtbl.create 42
     in
-    let relevant_wildcard_opt = function
-      | [] -> None
-      | wildcards -> Some (wildcards |> List.rev |> List.hd)
+    let narity_of_k = function
+      | Nil -> 0
+      | Int _ | Bool _ | String _ -> 1
+      | Cons -> 2
+      | Tuple n -> n
+      | Variant v -> Hashtbl.find ksttbl v
     in
-    let map_to_hashtable = 
-      fun ((pattern, expr):row) ->
-        match List.hd pattern with
+    let nary_wildcard n wildcards =
+      let rec inc_narity n ((wlist, expr):pattern list * source_expr) =
+        assert ( n >= 0);
+        if n > 0
+        then inc_narity (n-1) (Wildcard::wlist, expr)
+        else (wlist, expr)
+      in
+      List.map (inc_narity (n-List.length wildcards)) wildcards
+    in
+    let rec first_scan : pattern list -> unit = function
+      | [] -> ()
+      | (pattern::ptl) ->
+        match pattern with
+        | Wildcard -> ()
+        | Or (p1, p2) -> first_scan [p1; p2]
+        | As (p, _) -> first_scan [p]
         | Constructor (k, plist) ->
-          let binding = match Hashtbl.find_opt hashtbl k with
-            | Some clause_lst -> (plist, expr)::clause_lst
-            | None -> (plist, expr)::[]
+          match k with
+          | Variant v -> 
+            let len_ptl = List.length plist in
+            begin match Hashtbl.find_opt ksttbl v with 
+              | Some (narity:int) -> if len_ptl > narity then Hashtbl.replace ksttbl v len_ptl
+              | None -> Hashtbl.add ksttbl v len_ptl
+            end;
+            first_scan plist;
+            first_scan ptl
+          | _ -> () (* Don't consider constructors that are not variants *)
+    in
+    let rec put_in_group : pattern list * source_expr -> unit = function
+      | ([], _expr) -> assert false
+      | ((pattern::ptl), expr) ->
+        match pattern with
+        | Constructor (k, plist) ->
+          let binding = match Hashtbl.find_opt grouptbl (HConstructor k) with
+            | Some (idx, lst) -> (idx, (plist@ptl, expr)::lst)
+            | None -> let idx = Hashtbl.length grouptbl in
+              (idx, (plist@ptl, expr)::[])
           in
-          Hashtbl.replace hashtbl k binding
-        | Wildcard -> with_wildcard := (pattern, expr)::!with_wildcard
-        | _ -> ()
+          Hashtbl.replace grouptbl (HConstructor k) binding
+        | Wildcard ->
+          let binding = match Hashtbl.find_opt grouptbl HWildcard with
+          | Some (idx, lst) -> (idx, (ptl, expr)::lst)
+          | None -> let idx = Hashtbl.length grouptbl in
+            (idx, (ptl, expr)::[])
+          in
+          Hashtbl.replace grouptbl HWildcard binding
+        | As (pattern, _) -> put_in_group (pattern::ptl, expr)
+        | Or (p1, p2) -> put_in_group (p1::ptl, expr); put_in_group (p2::ptl, expr) 
     in
-    List.iter map_to_hashtable rows;
-    let fst = BatHashtbl.bindings hashtbl
-              |> List.map (fun (k, rows) ->
-                  (k, List.rev (!with_wildcard@rows)))
+    List.iter (fun row -> fst row |> first_scan) rows;
+    List.iter put_in_group rows;
+    let rec take_until_wildcards accum wildcards = function
+      | [] -> accum, wildcards
+      | hd::tl -> let (hk, (idx, rows)) = hd in
+        match hk with
+        | HWildcard ->  assert (wildcards = (0, [])); take_until_wildcards accum (idx, rows) tl
+        | HConstructor k -> take_until_wildcards ((k, rows)::accum) wildcards tl
     in
-    let snd = relevant_wildcard_opt !with_wildcard
+    let (k_rows, (last_idx, wildcards)) = BatHashtbl.bindings grouptbl
+                              |> List.sort (fun (_k, (idx, _rows)) (_, (idx', _)) -> Int.compare idx' idx)
+                              |> take_until_wildcards [] (0, [])
+    in
+    let fst = if (wildcards = []) then
+        k_rows
+      else
+        BatList.split_at last_idx k_rows
+        |> fst
+        |> List.map (fun (k, rows) -> let n = narity_of_k k in 
+                      (k, List.rev_append rows (nary_wildcard n wildcards)))
+    in
+    let snd = wildcards
     in
     (fst, snd)
-  in
+    in
   let rec decompose (rows: row list) : constraint_tree =
     match rows with
-    | ((pattern::ptl), expr)::tl ->
-      begin match pattern with
-        | Or (p1, p2) ->
-          decompose (((p1::ptl), expr) :: ((p2::ptl), expr) :: tl)
-        | Constructor (_, _) ->
-          let split, fallback = kst_rows_assoc rows
-          in
-          let split_evaluated =  split
-                                 |> List.map (fun (k, clause_lst) ->
-                                     k, decompose clause_lst)
-          in
-          let fallback_evaluated = match fallback with
-            | Some clause -> Some (decompose [clause])
-            | None -> None
-          in
-          Node (split_evaluated, fallback_evaluated)
-        | As (pattern, _) -> decompose (([pattern], expr)::tl)
-        | Wildcard -> ignore tl;
-          Leaf expr
-      end
     | [] -> assert false
     | ([], expr)::[] -> Leaf expr
-    | ([], expr)::tl -> ignore tl; Leaf expr (* TODO: DISCUSS: is it correct to ignore tail? *)
-  in
-  source.clauses |> List.map (fun (pattern, expr) -> ([pattern], expr)) |> decompose
-
+    | ([], expr)::tl -> ignore tl; Leaf expr
+    | ((_pattern::_ptl), _expr)::_tl ->
+      let groups, fallback = group_constructors rows
+      in
+      let groups_evaluated =  groups
+                              |> List.map (fun (k, clause_lst) ->
+                                  k, decompose clause_lst)
+      in
+      let fallback_evaluated = match fallback with
+        | [] -> Empty
+        | clause -> decompose clause
+      in
+      Node (groups_evaluated, fallback_evaluated)
+      (* TODO: DISCUSS: 
+       * Sometime groups_evaluated = []; Do we want to express that better? *)
+    in
+    source.clauses |> List.map (fun (pattern, expr) -> ([pattern], expr)) |> decompose
 
 let eval source_ast =
   let result = sym_exec source_ast in
