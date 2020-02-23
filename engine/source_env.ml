7b14c8e9241a7e6e43b3de0589fb3bd79bee04d7
Gabriel Scherer <gabriel.scherer@gmail.com>
2020-02-23 17:59:08 +0100

    associate type declarations, constructor declarations and target representation to constructors

---
+++ engine/source_env.ml
@@ -0,0 +1,33 @@
+open Ast
+
+module ConstructorMap = Map.Make(String)
+
+type type_env = (type_decl * constructor_decl) ConstructorMap.t
+
+let build_type_env type_decls =
+  let add_type_decl env type_decl =
+    let add_constructor env constructor =
+      ConstructorMap.add constructor.constructor_name (type_decl, constructor) env in
+    List.fold_left add_constructor env type_decl.constructors in
+  List.fold_left add_type_decl ConstructorMap.empty type_decls
+
+type constructor_repr =
+  | Int of int
+  | Tag of int
+
+type type_repr_env = constructor_repr ConstructorMap.t
+
+let build_type_repr_env type_decls =
+  let add_type_decl env type_decl =
+    let add_constructor (next_int, next_tag, env) constructor =
+      let next_int, next_tag, repr =
+        if constructor.args = [] then
+          (next_int + 1, next_tag, Int next_int)
+        else
+          (next_int, next_tag + 1, Tag next_tag)
+      in
+      (next_int, next_tag,
+       ConstructorMap.add constructor.constructor_name repr env) in
+    let (_, _, env) = List.fold_left add_constructor (0, 0, env) type_decl.constructors in
+    env in
+  List.fold_left add_type_decl ConstructorMap.empty type_decls
