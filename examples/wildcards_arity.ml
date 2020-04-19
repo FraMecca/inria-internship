66b50f6a79acf36752e2b58d3284ba725d0c40cb
Francesco Mecca <me@francescomecca.eu>
2020-04-19 19:27:27 +0200

    updated examples

--- examples/wildcards_arity.ml
+++ examples/wildcards_arity.ml
@@ -6,6 +6,6 @@ type k1 = K1 of int
 type k2 = K2 of int * int
 
 let mm = function
-  | (K1 1, _) -> observe 1 0
-  | (K1 1, K2 (2, 2)) -> observe 1 2 2
-  | _ -> observe 0
+  | (K1 1, K2 (2, 2)) -> observe [1; 2; 2;]
+  | (K1 1, _) -> observe [1;0]
+  | _ -> observe [0]
