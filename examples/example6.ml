55d019df8e5e89891733fa9335bf21ca9493494f
Francesco Mecca <me@francescomecca.eu>
2020-04-20 20:42:15 +0200

    updated example

--- examples/example6.ml
+++ examples/example6.ml
@@ -5,7 +5,7 @@ let mm = function
   | [(1, 2), 2]::[] -> observe ([(1, 2), 2]::[])
   | [(1, 2), 2]::_ -> observe [(1, 2), 2]
   | [(1, 2), x]::_ -> observe [(1, 2), x]
-  | [x]::[] -> observe x
-  | [x]::[y]::[] -> observe x y
-  | [x, y]::z -> observe x y z
+  | [x]::[] -> observe [x]
+  | [x]::[y]::[] -> observe [x, y]
+  | [x, y]::z -> observe [(x, y), z]
   | x -> observe x
